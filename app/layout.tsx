f0fb409f115ea3d0bff3a9050ea8b4030e02d8cb
Hassan <hassanahmad2809@gmail.com>
2024-03-08 14:42:48 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "../app/globals.css";
-import AnimatedCursor from "react-animated-cursor";
 
 export const metadata: Metadata = {
   title: "Hassan | Portfolio",
@@ -14,42 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <AnimatedCursor
-          innerSize={8}
-          outerSize={8}
-          color="107,0,191"
-          outerAlpha={0.2}
-          innerScale={0.7}
-          outerScale={5}
-          clickables={[
-            "a",
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            "label[for]",
-            "select",
-            "textarea",
-            "button",
-            ".link",
-            {
-              target: ".custom",
-              options: {
-                innerSize: 12,
-                outerSize: 12,
-                color: "107,0,191",
-                outerAlpha: 0.3,
-                innerScale: 0.7,
-                outerScale: 5,
-              } as any, // Use type assertion here
-            },
-          ]}
-        />
-
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,29 @@ import Skills from "./components/Skills";
 import { twMerge } from "tailwind-merge";
 import { TracingBeam } from "./components/ui/tracing-beam";
 import { Contact } from "./components/Contact";
+import AnimatedCursor from "react-animated-cursor";
 
 export default function Home() {
   return (
     <TracingBeam>
+      <div className="hidden lg:block absolute">
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={35}
+          innerScale={1}
+          outerScale={2}
+          outerAlpha={0}
+          // hasBlendMode={true}
+          // The library had an issue where the type wasnt described so i added it and it runs okay locally but not when deployed so i will later on submit a pull request to the github
+          innerStyle={{
+            backgroundColor: "rgb(124, 58 , 237)",
+          }}
+          outerStyle={{
+            border: "3px solid rgb(124, 58 , 237)",
+          }}
+          clickables={["a", "button", ".projectimg"]}
+        />
+      </div>
       <div>
         <Navbar />
         <Hero />
